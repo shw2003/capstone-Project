47453b18c59dc54884cbe58348bb3e7fa5f6702d
rmv4714 <shwetakamal4714@gmail.com>
2024-01-10 00:46:43 +0530

    ...

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
 } from "../../utils/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getRedirectResult(auth);
+        console.log(response);
+
+        if (response) {
+          const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     // const response = await signInWithGooglePopup();
@@ -12,17 +32,13 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    // const response = await signInWithGooglePopup();
-    console.log({ user });
-  };
-
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Popup
+      </button>
     </div>
   );
 };
