87d33aff8916353d2010462999048c41b2201b2c
shweta2003 <shwetakamal4714@gmail.com>
2024-02-14 14:25:25 +0530

    ..

---
+++ src/routes/categories-preview/categories-preview.components.jsx
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react";
+import { useContext } from "react";
+import { CategoriesContext } from "../../context/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import "./categories.styles.scss";
+import CategoryPreview from "../../components/category-preview/category-preview.components";
+
+const categoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        // <Fragment key={title}>
+        //   <h2>{title}</h2>
+        //   <div className="products-container">
+        //     {categoriesMap[title].map((product) => (
+        //       <ProductCard key={product.id} product={product} />
+        //     ))}
+        //   </div>
+        // </Fragment>
+        const products = categoriesMap[title];
+
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
+
+    // <div className="products-container">
+    //   {products.map((product) => (
+    //     <ProductCard key={product.id} product={product} />
+    //   ))}
+    // </div>
+  );
+};
+
+export default categoriesPreview;
